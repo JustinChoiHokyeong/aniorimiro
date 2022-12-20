ddefb68945a5986aed9fc4df041bb18d56100f2d
JustinChoiHokyeong <chhk7449@gmail.com>
2022-12-20 10:00:34 +0900

    분석+예측 기능 하나로 통합

--- aniorimiro/analysis/urls.py
+++ aniorimiro/analysis/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
   path('map/', views.map, name='map'), 
-  path('predict/', views.ListFunc, name='predict'),  
+
 ]
--- aniorimiro/analysis/views.py
+++ aniorimiro/analysis/views.py
@@ -4,43 +4,50 @@ import pandas as pd
 df=pd.read_csv("https://raw.githubusercontent.com/Kshinhye/aniorimiro_data/master/yongsan_192021.csv", encoding='utf-8')
 
 def map(request):
+  analData = df.head(2)
 
-  return render(request, 'analysis/map.html')
+  sub1 = request.GET.get('tradingArea')
+  sub2 = request.GET.get('businessType')
+
+  #if request.method == "POST":
+    #tradingArea = request.POST.get('tradingArea')
+    #businessType = request.POST.get('businessType')
+  context = {
+    'sub1': sub1,
+    'sub2': sub2,
+    #'tradingArea':tradingArea,
+    #'businessType':businessType
+    }
+  return render(request, 'analysis/map.html', context)
 
 
 
-def ListFunc(request):
-  sum = df
-  context = {'sum': sum}
-  return render(request,'analysis/predict.html', context)
-# 함수
+# def ListFunc(request):
+#   sum = df
+#   context = {'sum': sum}
+#   return render(request,'analysis/predict_test.html', context)
 
+# def mapDbFunc(request):
+#   if request.method=="GET":
+#     id=request.GET['query']
+#     data=df[df["상권코드명"]==id]
+#     # 이제 원하는대로 데이터를 뽑으면 됩니다. 수업에서 배웠듯이 jikdata생각하시면됩니다.
+#     data = {
+#       'id' : id,
+#       'data' : data
+#     }
+#   return render(request, 'analysis/map.html', data)
 
+# #1 상권구분 별 점포 수
+# def category(SG):
+#     GM=pd.DataFrame(df[df['상권구분코드명']==SG])
+#     result_SUM=GM["점포수"].sum()
+#     return result_SUM
 
-#1 상권구분 별 점포 수
-def category(SG):
-    GM=pd.DataFrame(df[df['상권구분코드명']==SG])
-    result_SUM=GM["점포수"].sum()
-    return result_SUM
+# #2 연령대별 매출 순위
+# def nai(NAI):
+#     strnai='연령대'+str(NAI)+'매출금액'
+#     test3=df.groupby(['서비스업종코드명']).sum()
+#     result= test3[strnai].sort_values(ascending=False)
+#     return result
 
-#2 연령대별 매출 순위
-def nai(NAI):
-    strnai='연령대'+str(NAI)+'매출금액'
-    test3=df.groupby(['서비스업종코드명']).sum()
-    result= test3[strnai].sort_values(ascending=False)
-    return result
-
-#3 상권별(서비스 업종) 점포수 ( 분기별 대비 )
-# CS100001 ~ CS100010 #외식업 WS
-# CS200001 ~ CS200037 #서비스업 SBS
-# CS300001 ~ CS300043 #소매업 SM
-
-#4 인기 업종 ( 분기별 증감률 )
-
-##매출 분석
-#5 점포별 월평균 매출액 (분기별 증감률 : 전국, 서울과 비교)
-#6 점포별 월평균 매출 건수 (분기별 증감률 : 전국, 서울과 비교)
-#7 요일별 매출분석 ( 할 수 있다면 서울시와 비교)
-#8 시간대별
-#9 성별별 ( + 각 업종마다 성별의 매출비교 )
-#10 연령대별 각업종 매출비율
