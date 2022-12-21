3b8dda2f1afe580d8c0fd1265bf2ae25c5dd0644
JustinChoiHokyeong <chhk7449@gmail.com>
2022-12-21 14:41:24 +0900

    상권, 업종 선택 카테고리 기능 추가

--- aniorimiro/analysis/views.py
+++ aniorimiro/analysis/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 import pandas as pd
-import asyncio
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 df=pd.read_csv("https://raw.githubusercontent.com/Kshinhye/aniorimiro_data/master/yongsan_192021.csv", encoding='utf-8')
 
@@ -8,50 +9,28 @@ df=pd.read_csv("https://raw.githubusercontent.com/Kshinhye/aniorimiro_data/maste
 def map(request):
   analData = df.head(2)
 
-  sub1 = request.GET.get('tradingArea')
-  sub2 = request.GET.get('businessType')
-
-  #if request.method == "POST":
-    #tradingArea = request.POST.get('tradingArea')
-    #businessType = request.POST.get('businessType')
-  context = {
-    'sub1': sub1,
-    'sub2': sub2,
-    #'tradingArea':tradingArea,
-    #'businessType':businessType
-    }
-  return render(request, 'analysis/map.html', context)
+  return render(request, 'analysis/map.html')
 
 
+def calldbFunc(request):
+    if request.method=="POST":
+        print('view옴')
+        tradingArea=request.POST.get('tradingArea')
+        BigTradingArea=request.POST.get('BigTradingArea')
+        businessType=request.POST.get('businessType')
+        smallBusiType=request.POST.get('smallBusiType')
+        context = {
+          'businessType':businessType,
+          'tradingArea':tradingArea,
+          'BigTradingArea':BigTradingArea,
+          'smallBusiType':smallBusiType
+        }
+        
+    return JsonResponse(context)
 
-# def ListFunc(request):
-#   sum = df
-#   context = {'sum': sum}
-#   return render(request,'analysis/predict_test.html', context)
 
-# def mapDbFunc(request):
-#   if request.method=="GET":
-#     id=request.GET['query']
-#     data=df[df["상권코드명"]==id]
-#     # 이제 원하는대로 데이터를 뽑으면 됩니다. 수업에서 배웠듯이 jikdata생각하시면됩니다.
-#     data = {
-#       'id' : id,
-#       'data' : data
-#     }
-#   return render(request, 'analysis/map.html', data)
 
-# #1 상권구분 별 점포 수
-# def category(SG):
-#     GM=pd.DataFrame(df[df['상권구분코드명']==SG])
-#     result_SUM=GM["점포수"].sum()
-#     return result_SUM
 
-# #2 연령대별 매출 순위
-# def nai(NAI):
-#     strnai='연령대'+str(NAI)+'매출금액'
-#     test3=df.groupby(['서비스업종코드명']).sum()
-#     result= test3[strnai].sort_values(ascending=False)
-#     return result
 
 
   
